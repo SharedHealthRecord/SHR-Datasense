ae94f270f429043c96327a7affa3eae73d104e09
Utsab Banerjee <utsabban@thoughtworks.com>
2015-02-05 16:11:30 +0530

    Utsab, Hitansu | fixing build

--- src/test/java/org/sharedhealth/datasense/helpers/DatabaseHelper.java
+++ src/test/java/org/sharedhealth/datasense/helpers/DatabaseHelper.java
@@ -17,6 +17,6 @@ public class DatabaseHelper {
         template.update("delete from patient", new EmptySqlParameterSource());
         template.update("delete from encounter", new EmptySqlParameterSource());
         template.update("delete from drug", new EmptySqlParameterSource());
-        template.update("delete from procedure", new EmptySqlParameterSource());
+        template.update("delete from procedures", new EmptySqlParameterSource());
     }
 }
