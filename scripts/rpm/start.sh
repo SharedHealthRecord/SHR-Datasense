fd68c45ddb1f50f31719b508edcdebb3ab44b32e
Sanjana Bayya <sanjanb@thoughtworks.com>
2015-03-24 11:05:16 +0530

    Sanjana | #657 | Setting up log4j configuration for the application.

--- scripts/rpm/start.sh
+++ scripts/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java -Dserver.port=$DATASENSE_PORT -jar /opt/datasense/lib/datasense-0.1-SNAPSHOT.war > /var/log/datasense/datasense.log &
+nohup java -Dserver.port=$DATASENSE_PORT -jar /opt/datasense/lib/datasense-0.1-SNAPSHOT.war > /dev/null 2>&1  &
 echo $! > /var/run/datasense/datasense.pid
