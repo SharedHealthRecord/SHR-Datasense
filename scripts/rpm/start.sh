1eaff320abd147acf9a31019ad7f0c7b563cdff5
rnjn <runjan@gmail.com>
2015-04-09 15:10:01 +0530

    Adding log level configurablity

--- scripts/rpm/start.sh
+++ scripts/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java -Dserver.port=$DATASENSE_PORT -jar /opt/datasense/lib/datasense-0.1-SNAPSHOT.war > /dev/null 2>&1  &
+nohup java -Dserver.port=$DATASENSE_PORT -DDATASENSE_LOG_LEVEL=$DATASENSE_LOG_LEVEL -jar /opt/datasense/lib/datasense-0.1-SNAPSHOT.war > /dev/null 2>&1  &
 echo $! > /var/run/datasense/datasense.pid
