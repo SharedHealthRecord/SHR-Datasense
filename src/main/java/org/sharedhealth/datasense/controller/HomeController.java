a3ad17b3c1de608d9d8a566cd9ded4ddba97f350
angshu <angshuonline@gmail.com>
2015-06-02 11:55:33 +0530

    763 | fixing label and form layout

--- src/main/java/org/sharedhealth/datasense/controller/HomeController.java
+++ src/main/java/org/sharedhealth/datasense/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = {"/home" }, method = RequestMethod.GET)
+    @RequestMapping(value = {"/home", "/", ""}, method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('ROLE_SHR System Admin')")
     public String home() {
         return "home";
