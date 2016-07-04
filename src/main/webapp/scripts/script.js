1ede2737424f50b0a3944b0451b567f10fb84053
anjalyj <anjalyj@thoughtworks.com>
2016-07-04 17:41:32 +0530

    sukreet, anjaly | BDSHR-1309 | changing appearance of the home page.

--- src/main/webapp/scripts/script.js
+++ src/main/webapp/scripts/script.js
@@ -25,3 +25,22 @@ var loadComponents = function() {
 }
 
 $(document).ready(loadComponents);
+
+$(document).ready(function () {
+    $(function(){
+        var current_page_URL = location.href;
+
+        $( "a" ).each(function() {
+
+            if ($(this).attr("href") !== "#") {
+
+                var target_URL = $(this).prop("href");
+
+                    if (target_URL == current_page_URL) {
+                        $('nav a').parents('li, ul').removeClass('active');
+                        $(this).parent('li').addClass('active');
+
+                        return false;
+                    }
+            }
+        }); }); });
